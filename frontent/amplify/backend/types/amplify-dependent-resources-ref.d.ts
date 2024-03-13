22a95c8f370cea77b7f8cec5bfb0ae49829705fd
Mehedi hasan <mrmehedihaasan@gmail.com>
2024-03-13 21:49:37 +0600

    amplify

---
+++ frontent/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "frontent": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- frontent/src/pages/_app.tsx
+++ frontent/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import '../../amplifyConfigure'
 import type { AppProps } from "next/app";
 import StoreProvider from '../Redux/provider';
 
